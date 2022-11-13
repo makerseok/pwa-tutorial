95c0b04554e9e90fbd0e59b1f4bba89c3b3b1910
makerseok <maker7788@gmail.com>
2022-11-13 15:42:36 +0900

    pre-caching assets

--- sw.js
+++ sw.js
@@ -1,14 +1,33 @@
+const staticCashName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+];
+
 // install event
 self.addEventListener('install', evt => {
-  console.log('service worker has been installed');
+  // console.log('service worker has been installed');
+  evt.waitUntil(
+    caches.open(staticCashName).then(cache => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    }),
+  );
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-  console.log('service worker has been activated');
+  // console.log('service worker has been activated');
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  // console.log('fetch event', evt);
 });
